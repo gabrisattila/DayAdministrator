3710a494873804056035f6cb10e57d816929dd48
Gábris Attila <gabris.attila.hun@gmail.com>
2024-03-25 22:51:36 +0100

    Example day string

---
+++ src/main/java/Classes/Parser/package-info.java
@@ -0,0 +1,11 @@
+package Classes.Parser;
+
+/*
+* Basic day string:
+* 610-710 reggel; 86,5 kg; 1 kávé;
+* 710-810 ut. - 30 olv.; 810-1205 meló;
+* 1205-25 ebéd; 1 kávé; 1235-16 meló; 16-17 ut. - 25 olv. - 10 olv.;
+* 17-1730 vacsi; 1 kávé; 18-1930 DayAdmin; 20-2130 ...Videó // Bekategorizálás megírni.
+* 2130-23 írás; 23-2330 saláta és ebéd elrakni; 2330-24 fekvés;
+* 10 cigi;
+* */
